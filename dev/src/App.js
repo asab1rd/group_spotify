f11f923d66b36600b8523ed90b448c460a964d6a
Belinda Boudra <belinda.boudra@epitech.eu>
2020-03-01 15:00:35 +0100

    bebe

--- dev/src/App.js
+++ dev/src/App.js
@@ -8,6 +8,7 @@ function App() {
       <Header />
       <Home />
       <Footer />
+      <div />
     </div>
   );
 }
---
+++ dev/src/components/getTracks.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+//import { myfunction, myotherfunction } from './mondossier'
+function getTracks() {
+    const content = "Salut, moi je suis le tracks.";
+    return (
+        <div className="getTracks">
+            {content}
+        </div>
+    )
+}
+export default getTracks
